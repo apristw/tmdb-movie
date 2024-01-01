df10065bcd082329eb5518c4bb61926bb126ecdb
apristw <mailapriawan@gmail.com>
2024-01-02 01:01:42 +0700

    menambahkan fitur slider pada popular movies, menambahkan fitur rating pada Card components

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
 import React from "react";
-import genreMovie from "../utils/genre";
 
-function Card({ title, release_date, genre, image }) {
-  const genreList = genreMovie;
-  console.log(genreMovie);
-
+function Card({ title, release_date, genre, image, rating }) {
   return (
     <>
-      <div className="h-[490px] w-[250px] cursor-pointer scale-95 hover:scale-100 transition-all duration-200 ease-in-out">
+      <div className="max-h-[490px] max-w-[250px] cursor-pointer scale-[98%] hover:scale-[102%] transition-all duration-200 ease-in-out">
         <div className="relative max-w-[250px] w-full h-[370px]">
           <Image
             src={`${process.env.NEXT_PUBLIC_IMG_URL_POSTER}/${image}`}
@@ -19,10 +15,13 @@ function Card({ title, release_date, genre, image }) {
             priority
           />
         </div>
-        <div className="">
+        <div className="relative">
+          <p className="absolute right-0 mt-1 py-1 px-3 font-bold text-sm rounded-md bg-yellow-500">
+            {rating.toFixed(1)}
+          </p>
           <p className="text-sm text-gray-400 py-2">{release_date}</p>
           <p className="font-bold mb-2">{title}</p>
-          <p>{genre}</p>
+          <p className="text-sm text-gray-400">{genre}</p>
         </div>
       </div>
     </>
