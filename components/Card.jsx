c5e045da25d89eed893eac3320727327482fb293
apristw <mailapriawan@gmail.com>
2024-01-03 00:17:38 +0700

    Merubah fetching data dengan membuat file baru api.js sehingga data fetch bisa digunakan berulang, dan membuat reusable komponen pada MovieSlider

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import React from "react";
 
-function Card({ title, release_date, genre, image, rating }) {
+function Card({ title, release_date, genres, genre_ids, image, rating }) {
+  const movieGenres = genre_ids.map((genreId) => {
+    const genreObj = genres.find((item) => item.id === genreId);
+    return genreObj ? genreObj.name : "";
+  });
+
   return (
     <>
-      <div className="max-h-[490px] max-w-[250px] cursor-pointer scale-[98%] hover:scale-[102%] transition-all duration-200 ease-in-out">
+      <div className="max-h-[490px] max-w-[250px] cursor-pointer scale-[98%] hover:scale-[100%] transition-all duration-200 ease-in-out">
         <div className="relative max-w-[250px] w-full h-[370px]">
           <Image
             src={`${process.env.NEXT_PUBLIC_IMG_URL_POSTER}/${image}`}
@@ -21,7 +26,7 @@ function Card({ title, release_date, genre, image, rating }) {
           </p>
           <p className="text-sm text-gray-400 py-2">{release_date}</p>
           <p className="font-bold mb-2">{title}</p>
-          <p className="text-sm text-gray-400">{genre}</p>
+          <p className="text-sm text-gray-400">{movieGenres.join(", ")}</p>
         </div>
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,15 @@
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import PopularMovie from "@/components/PopularMovie";
-import Card from "@/components/Card";
+import MovieSlider from "@/components/MovieSlider";
 
 export default function Home() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(`${process.env.NEXT_PUBLIC_TMDB_BASE_URL}/movie/popular`, {
-        params: {
-          api_key: process.env.NEXT_PUBLIC_TMDB_KEY,
-        },
-      })
-      .then((response) => {
-        // console.log(response.data.results);
-        setMovies(response.data.results);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
-
   return (
     <>
       <Navigation />
-      <Hero data={movies} />
-      <PopularMovie data={movies} />
-      {/* <Card /> */}
-      {/* {movies.map((data, index) => {
-        return <p key={index}>{data.title}</p>;
-      })} */}
+      <Hero endpoint={`/movie/popular`} />
+      <MovieSlider endpoint={`/movie/popular`} title={"Popular Movie"} />
+      <MovieSlider endpoint={`/trending/movie/day`} title={"Trending Today"} />
+      <MovieSlider endpoint={`/movie/top_rated`} title={"Top Rated"} />
     </>
   );
 }
